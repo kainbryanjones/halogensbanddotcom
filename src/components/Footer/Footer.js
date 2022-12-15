2a3308b0c263ba97711d515a64e174cf0c8b38c4
Kain Bryan-Jones <89807193+kainbryanjones@users.noreply.github.com>
2022-12-15 18:15:05 +0000

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,9 +9,9 @@ const Footer = () => {
             <div className="footer-social">
                 <SocialIcon  className="footer-icon" network="youtube" bgColor="transparent" fgColor="white" url="https://www.youtube.com/@halogensband" />
                 <SocialIcon className="footer-icon" network="instagram" bgColor="transparent" fgColor="white" url="https://instagram.com/halogensband"/>
-                <SocialIcon className="footer-icon" network="spotify" bgColor="transparent" fgColor="white" />
+                <SocialIcon className="footer-icon" network="spotify" bgColor="transparent" fgColor="white" url="https://open.spotify.com/artist/3RAETi9Ml0yLpZmfRrnm0C"/>
                 <SocialIcon className="footer-icon" network="soundcloud" bgColor="transparent" fgColor="white" url="https://soundcloud.com/user-403925093"/>
-                <SocialIcon className="footer-icon" network="itunes" bgColor="transparent" fgColor="white" />
+        {/*<SocialIcon className="footer-icon" network="itunes" bgColor="transparent" fgColor="white" />*/}
             </div>
             <div className="footer-content">
             </div>
