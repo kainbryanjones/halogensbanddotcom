eca0760ad28bf181a0b73c9faccd6ea318c3f861
Kain <kainbryanjones.misc@gmail.com>
2022-12-25 18:43:06 +0000

    Ready to go to Next js

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -6,20 +6,12 @@ const Home = () => {
 
     useDocumentTitle("Halogens")
 
-    /**
-     * 
-     *             <Ratio aspectRatio="16x9">
-                <iframe width="1120" height="630" src={`https://www.youtube.com/embed/${youtubeId}?controls=0`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-            </Ratio>
-     */
-
     const YoutubeVideo = ({ youtubeId }) => {
         return (
             <iframe id="yt-player-featured" src={`https://www.youtube-nocookie.com/embed/${youtubeId}?enablejsapi=1`} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen="" title="Halogens - Motorbike(Official Visualiser)" width="560" height="315" frameBorder="0"></iframe>
         );
     };
 
-    
     return (
         <div className="home-container">
             <div className="title">
