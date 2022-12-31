65a082083e7172bcb831fc77b7d09212ce00d2f1
Kain <kainbryanjones.misc@gmail.com>
2022-12-31 11:30:26 +0000

    added sentry

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+Sentry.init({
+    dsn: "https://9e029bb23722478d85d1d30fd65c8cc4@o4504019617710080.ingest.sentry.io/4504423076397056",
+    integrations: [new BrowserTracing()],
+  
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
 
 root.render(
     <React.StrictMode>
